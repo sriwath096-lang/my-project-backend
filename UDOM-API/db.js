34002107561b81052cd5910c910db769ebda64b4
sriwath096-lang <sriwath096@gmail.com>
2026-09-07 15:05:52 +0700

    Add files via upload

--- UDOM-API/db.js
+++ UDOM-API/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: 3306,
+  charset: 'utf8mb4',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
